5e3583ec9556b68d2f3ec081de557c0cbf74412f
dijk4tra <dijk4tra@gmail.com>
2026-03-15 22:44:30 +0800

    Day16

--- Day16_CStageHomeWork/Q07_读写二进制数据/main.c
+++ Day16_CStageHomeWork/Q07_读写二进制数据/main.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 /*
 在网盘项目中，常需要保存文件的元数据（如文件名、大小）。
@@ -10,7 +11,40 @@
     3. 使用 `fread()` 将数据读回到新的结构体变量中并输出。
 */
 
+// 1.定义结构体 `FileInfo`
+typedef struct {
+    char filename[64]; // 文件名
+    long filesize;     // 文件大小（单位：字节）
+} FileInfo;
+
 int main(void) {
-    
+    FileInfo write_data = { "lesson_by_kongling.mp4",114514 }; // 初始化一个结构体变量
+    FileInfo read_data = { 0 }; // 创建一个空的结构体，用于接收读取的数据
+
+    FILE *fp_out = fopen("index.db", "wb");
+    if (fp_out == NULL) {
+        perror("fopen failed");
+        return 1;
+    }
+
+    // 2.使用 `fwrite()` 将结构体变量整体写入 `index.db`
+    fwrite(&write_data, sizeof(FileInfo), 1, fp_out);
+    fclose(fp_out);
+    printf("fwrite succeeded.\n");
+
+    FILE *fp_in = fopen("index.db", "rb");
+    if (fp_in == NULL) {
+        perror("fopen failed");
+        return 1;
+    }
+    //3.使用 `fread()` 将数据读回到新的结构体变量中并输出
+    if ((fread(&read_data, sizeof(FileInfo), 1, fp_in)) == 1) {
+        printf("\nfread succeeded: \n");
+        printf("filename: %s\n", read_data.filename);
+        printf("filesize: %ld\n", read_data.filesize);
+    }
+
+    fclose(fp_in);
+
     return 0;
 }
